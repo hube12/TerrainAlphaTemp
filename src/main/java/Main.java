b34bc0897a5b1b3d20bf6a2a682af7b0201ac47a
Neil <H66r10@gmail.com>
2020-03-26 06:39:00 +0100

    Threading dispatch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,6 +41,71 @@ public class Main implements Runnable {
         files.add(new Pair<>("seeds60.txt", 60));
         files.add(new Pair<>("seeds62.txt", 62));
         files.add(new Pair<>("seeds63.txt", 63));
+        // ALL above are already have been runned
+        // ALL below are on GCP instance
+        files.add(new Pair<>("blob_seeds101.txt", 101));
+        files.add(new Pair<>("blob_seeds102.txt", 102));
+        files.add(new Pair<>("blob_seeds103.txt", 103));
+        files.add(new Pair<>("blob_seeds104.txt", 104));
+        files.add(new Pair<>("blob_seeds105.txt", 105));
+        files.add(new Pair<>("blob_seeds108.txt", 108));
+        files.add(new Pair<>("blob_seeds119.txt", 119));
+        files.add(new Pair<>("blob_seeds124.txt", 124));
+        files.add(new Pair<>("blob_seeds133.txt", 133));
+        files.add(new Pair<>("blob_seeds135.txt", 135));
+        files.add(new Pair<>("blob_seeds149.txt", 149));
+        files.add(new Pair<>("blob_seeds150.txt", 150));
+        files.add(new Pair<>("blob_seeds151.txt", 151));
+        files.add(new Pair<>("blob_seeds152.txt", 152));
+        files.add(new Pair<>("blob_seeds153.txt", 153));
+        files.add(new Pair<>("blob_seeds154.txt", 154));
+        files.add(new Pair<>("blob_seeds156.txt", 156));
+        files.add(new Pair<>("blob_seeds165.txt", 165));
+        files.add(new Pair<>("blob_seeds172.txt", 172));
+        files.add(new Pair<>("blob_seeds23.txt", 23));
+        files.add(new Pair<>("blob_seeds24.txt", 24));
+        files.add(new Pair<>("blob_seeds26.txt", 26));
+        files.add(new Pair<>("blob_seeds27.txt", 27));
+        files.add(new Pair<>("blob_seeds38.txt", 38));
+        files.add(new Pair<>("blob_seeds40.txt", 40));
+        files.add(new Pair<>("blob_seeds41.txt", 41));
+        files.add(new Pair<>("blob_seeds42.txt", 42));
+        files.add(new Pair<>("blob_seeds43.txt", 43));
+        files.add(new Pair<>("blob_seeds53.txt", 53));
+        files.add(new Pair<>("blob_seeds55.txt", 55));
+        files.add(new Pair<>("blob_seeds59.txt", 59));
+        files.add(new Pair<>("blob_seeds6.txt", 6));
+        files.add(new Pair<>("blob_seeds60.txt", 60));
+        files.add(new Pair<>("blob_seeds7.txt", 7));
+        files.add(new Pair<>("blob_seeds70.txt", 70));
+        files.add(new Pair<>("blob_seeds73.txt", 73));
+        files.add(new Pair<>("blob_seeds74.txt", 74));
+        files.add(new Pair<>("blob_seeds76.txt", 76));
+        files.add(new Pair<>("blob_seeds87.txt", 87));
+        files.add(new Pair<>("blob_seeds88.txt", 88));
+        files.add(new Pair<>("blob_seeds89.txt", 89));
+        files.add(new Pair<>("blob_seeds9.txt", 9));
+        files.add(new Pair<>("blob_seeds90.txt", 90));
+        files.add(new Pair<>("blob_seeds92.txt", 92));
+
+        files.add(new Pair<>("seeds108.txt", 108));
+        files.add(new Pair<>("seeds110.txt", 110));
+        files.add(new Pair<>("seeds124.txt", 124));
+        files.add(new Pair<>("seeds126.txt", 126));
+        files.add(new Pair<>("seeds141.txt", 141));
+        files.add(new Pair<>("seeds143.txt", 143));
+        files.add(new Pair<>("seeds156.txt", 156));
+        files.add(new Pair<>("seeds157.txt", 157));
+        files.add(new Pair<>("seeds158.txt", 158));
+        files.add(new Pair<>("seeds172.txt", 172));
+        files.add(new Pair<>("seeds76.txt", 76));
+        files.add(new Pair<>("seeds78.txt", 78));
+        files.add(new Pair<>("seeds79.txt", 79));
+        files.add(new Pair<>("seeds92.txt", 92));
+        files.add(new Pair<>("seeds93.txt", 93));
+
+
+
 
         for (int i = 0; i < 12; i++) {
             Thread thread = new Thread(new Main(files.get(i).getKey(), files.get(i).getValue()));
